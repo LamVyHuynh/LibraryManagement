effe81ac92c4c68b0ee370b0f235aa4aefd04c2a
LamVyHuynh <huynhlamvy145@gmail.com>
2026-01-07 13:36:42 +0700

    Khởi tạo cấu trúc sách

---
+++ Book.java
@@ -0,0 +1,12 @@
+public class Book {
+    private String id;
+    private String title;
+    private String author;
+
+    // Thêm Constructor để sau này dùng cho tiện
+    public Book(String id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+}
